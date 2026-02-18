43876a615a0ce2324d7417fb222de9a07d887ddb
DIOGO VIEIRA DA SILVA <05708376007@senacrs.edu.br>
2026-02-18 11:28:30 -0300

    Criando função funcional com resultado baseado no bmi e ligada ao html

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const btn = document.getElementById('btn-calc');
+ const btn = document.getElementById('btn-calc');
 
 btn.addEventListener('click', function() {
     
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
 
     if (weight > 0 && height > 0) {
         
-          const heightInMeters = height / 100;
-        const bmi = weight/(heightInMeters * heightInMeters);
- const category = getBmiCategory(bmi);
-        
+        const heightInMeters = height / 100;
+        const bmi = weight / (heightInMeters * heightInMeters);
+
+        const category = getBmiCategory(bmi);
+
         document.getElementById('bmi-value').innerText = bmi.toFixed(2);
+        document.getElementById('description').innerText = "Status: " + category;
+
     } else {
         alert("Please fill in all fields!");
     }
 
-    function getBmiCategory(bmi) {
+});
+
+function getBmiCategory(bmi) {
     if (bmi < 18.5) {
         return "Underweight";
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
+    } else if (bmi <= 24.9) {
         return "Normal weight (Eutrophic)";
-    } else if (bmi >= 25.0 && bmi <= 29.9) {
+    } else if (bmi <= 29.9) {
         return "Overweight (Pre-obesity)";
-    } else if (bmi >= 30.0 && bmi <= 34.9) {
+    } else if (bmi <= 34.9) {
         return "Obesity Class I";
-    } else if (bmi >= 35.0 && bmi <= 39.9) {
+    } else if (bmi <= 39.9) {
         return "Obesity Class II (Severe)";
     } else {
         return "Obesity Class III (Morbid or Severe)";
     }
 }
-
-});
