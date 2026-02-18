c57b2b54ca4ce29e9536eecf59cf3b95fdf56477
DIOGO VIEIRA DA SILVA <05708376007@senacrs.edu.br>
2026-02-18 11:26:00 -0300

    Adicionando descrição do status e categoria baseada no resultado do bmi

--- script.js
+++ script.js
@@ -9,10 +9,27 @@ btn.addEventListener('click', function() {
         
           const heightInMeters = height / 100;
         const bmi = weight/(heightInMeters * heightInMeters);
-
+ const category = getBmiCategory(bmi);
         
         document.getElementById('bmi-value').innerText = bmi.toFixed(2);
     } else {
         alert("Please fill in all fields!");
     }
+
+    function getBmiCategory(bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "Normal weight (Eutrophic)";
+    } else if (bmi >= 25.0 && bmi <= 29.9) {
+        return "Overweight (Pre-obesity)";
+    } else if (bmi >= 30.0 && bmi <= 34.9) {
+        return "Obesity Class I";
+    } else if (bmi >= 35.0 && bmi <= 39.9) {
+        return "Obesity Class II (Severe)";
+    } else {
+        return "Obesity Class III (Morbid or Severe)";
+    }
+}
+
 });
